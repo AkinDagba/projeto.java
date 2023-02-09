f119aaed60d6ad702a93cae7263d25290296ddb9
AkinDagba <103046273+AkinDagba@users.noreply.github.com>
2023-02-09 10:34:43 -0300

    Add files via upload

--- Projeto_LPOO/src/projetinho/Aluno.java
+++ Projeto_LPOO/src/projetinho/Aluno.java
@@ -3,11 +3,14 @@ package projetinho;
 public class Aluno extends Pessoa {
 
 	private String matricula;
+	private String curso;
+	private String anoDeEntrada;
 	
-	public Aluno(String nome, String cpf, String matricula) 
+	public Aluno(String nome, String cpf, String matricula, String curso, String anoDeEntrada) 
 	{
 		super(nome, cpf);	
 		this.setMatricula(matricula);
+		this.setCurso(curso);
 	}
 
 	public String getMatricula() {
@@ -16,6 +19,22 @@ public class Aluno extends Pessoa {
 
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getAnoDeEntrada() {
+		return anoDeEntrada;
+	}
+
+	public void setAnoDeEntrada(String anoDeEntrada) {
+		this.anoDeEntrada = anoDeEntrada;
 	}
 
 }
